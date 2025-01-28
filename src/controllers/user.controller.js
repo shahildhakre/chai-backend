1aec97055890c0daf04f830b8c7e99512b3be4c0
Sahil Dhakre <21ucs186@lnmiit.ac.in>
2025-01-28 15:19:28 +0530

    register controller debugging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ const registerUser = asyncHandler( async (req, res) => {
     // return response
 
     const {username, email, fullName, password} = req.body
-    console.log("email: ", email); // debugging using postman
+    // console.log("email: ", email); // debugging using postman
 
     // if(fullName == "") {
     //     throw new ApiError(400, "FullName is required")
@@ -31,16 +31,22 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{email}, {username}]
     })
 
     if(existedUser){
         throw new ApiError(409, "User with email or username already exist")
     }
+    // console.log(req.files);
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // alternate method to check for coverImageLocalPath
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar is required");
